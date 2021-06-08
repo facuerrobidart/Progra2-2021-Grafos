2e6b75ed73c349931422fda700bbb06c2c213b82
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-06-08 12:37:57 -0300

    errores corregidos

--- ej8/main.c
+++ ej8/main.c
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define max 20
+
+typedef struct nodo{
+    int etiqueta;
+    struct nodo *sig;
+}nodo;
+typedef nodo *TLista;
+typedef struct{
+    int etiqueta;
+    TLista adyacentes;
+}cabeceras;
 int main(){
 
     return 0;
@@ -9,7 +19,7 @@ int main(){
 /* Desarrollar funciones recursivas para:
 a) generar un arreglo con el grado de cada vértice de un grafo almacenado en una matriz de
 adyacencia.
-b) hallar el vértice con el mayor grado de salida, en un digrafo representado por una matriz
+b) hallar el vértice con el mayor grado de entrada, en un digrafo representado por una matriz
 de adyacencia.
 c) hallar el vértice con el mayor grado de entrada, en un digrafo representado por una lista de
 adyacencia.
@@ -18,25 +28,27 @@ e) generar un vector de registros con grado y vértice para aquellos vértices, 
 almacenado en una matriz de adyacencia, cuyo grado sea mayor a 3.*/
 
 void generaArreglo(int matriz[][max],int grados[],int i,int j,int orden){ //i,j entran con orden-1
-    if (i>=0 && j>=0){
-        if (matriz[i][j]){
-            grados[i]++; grados[j]++; //como solo recorro el triangulo superior tengo que sumar grado de i y de j
-        }
-        if (i==j) //si estoy parado sobre la diagonal, comienzo la fila de arriba 
-            generaArreglo(matriz,grados,i-1,orden-1,orden);
-        else //si no, me desplazo a la izquierda en la misma fila
+    if (i>0){
+        if (j>0){
+            if (matriz[i][j])
+                grados[i]++;
             generaArreglo(matriz,grados,i,j-1,orden);
+        }else{
+            if (matriz[i][j])
+                grados[i];
+            generaArreglo(matriz,grados,i-1,orden-1,orden);
+        }
     }
 }
 
-int gradoSalida(int matriz[][max],int vertice,int j){ //j entra con orden-1
-    if (j>=0) //debo recorrer la fila del vertice
-        if (matriz[vertice-1][j])
-            return 1+gradoSalida(matriz,vertice,j-1);
-        else
-            return gradoSalida(matriz,vertice,j-1);
-    else
-        return 0;
+int gradoEntradaLista(cabeceras listaAdy[max],int vertice,int i, TLista aux){ //i entra con orden-1, aux en null
+    if (i>=0){
+        if (aux=NULL){
+            return gradoEntradaLista(listaAdy,vertice,i-1,listaAdy[i-1].adyacentes);
+        }else{
+            
+        }
+    }
 }
 
 int gradoEntrada(int matriz[][max],int vertice,int i){//i entra con orden-1
