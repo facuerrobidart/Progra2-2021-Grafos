59f561f06bc6c0356f57806a700369ef298b6e8d
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-06-08 16:35:13 -0300

    utiliza premium maestro PREMIUMMM

--- ej8/main.c
+++ ej8/main.c
@@ -11,6 +11,10 @@ typedef struct{
     int etiqueta;
     TLista adyacentes;
 }cabeceras;
+typedef struct{
+    int grado;
+    int vertice;
+}TArray;
 int main(){
 
     return 0;
@@ -18,48 +22,61 @@ int main(){
 
 /* Desarrollar funciones recursivas para:
 a) generar un arreglo con el grado de cada vértice de un grafo almacenado en una matriz de
-adyacencia.
-b) hallar el vértice con el mayor grado de entrada, en un digrafo representado por una matriz
-de adyacencia.
-c) hallar el vértice con el mayor grado de entrada, en un digrafo representado por una lista de
-adyacencia.
-
-e) generar un vector de registros con grado y vértice para aquellos vértices, de un grafo
-almacenado en una matriz de adyacencia, cuyo grado sea mayor a 3.*/
+adyacencia.*/
 
 void generaArreglo(int matriz[][max],int grados[],int i,int j,int orden){ //i,j entran con orden-1
-    if (i>0){
+    if (i>=0){
         if (j>0){
             if (matriz[i][j])
                 grados[i]++;
             generaArreglo(matriz,grados,i,j-1,orden);
         }else{
             if (matriz[i][j])
-                grados[i];
+                grados[i]++;
             generaArreglo(matriz,grados,i-1,orden-1,orden);
         }
     }
 }
 
-int gradoEntradaLista(cabeceras listaAdy[max],int vertice,int i, TLista aux){ //i entra con orden-1, aux en null
+/*b) hallar el vértice con el mayor grado de entrada, en un digrafo representado por una matriz
+de adyacencia.*/
+
+void mayorGradoMatriz(int matriz[][max],int i,int j,int orden,int gAct,int *vMax,int *gMax){
+    if (j>=0)
+        if (i==0){
+            if (matriz[i][j] && gAct+1>*gMax){
+                *gMax=gAct+1;
+                *vMax=j+1;
+            }
+            mayorGradoMatriz(matriz,orden-1,j-1,orden,0,vMax,gMax); //comienzo a contar el grado de entrada de otro nodo
+        }else
+            if (matriz[i][j])
+                mayorGradoMatriz(matriz,i-1,j,orden,gAct+1,vMax,gMax);
+            else
+                mayorGradoMatriz(matriz,i-1,j,orden,gAct,vMax,gMax);
+}
+/*c) hallar el vértice con el mayor grado de entrada, en un digrafo representado por una lista de
+adyacencia.*/
+
+void mayorEntradaLista(cabeceras lista[max],int grados[max],TLista aux,int i,int orden,int *gMax,int *vMax){//i entra como ORDEN
     if (i>=0){
-        if (aux=NULL){
-            return gradoEntradaLista(listaAdy,vertice,i-1,listaAdy[i-1].adyacentes);
+        if (aux==NULL){ //este es el ultimo elemento de cada elemento del vector
+            mayorEntradaLista(lista,grados,lista[i-1].adyacentes,i-1,orden,gMax,vMax);
+            if (i==0){
+                *gMax=grados[i];
+                *vMax=i+1;
+            }else
+                if (i<orden && grados[i]>gMax){
+                    *gMax=grados[i];
+                    *vMax=i+1;
+                }
         }else{
-            
+            grados[aux->etiqueta]++;
+            mayorEntradaLista(lista,grados,aux->sig,i,orden,gMax,vMax);
         }
     }
 }
 
-int gradoEntrada(int matriz[][max],int vertice,int i){//i entra con orden-1
-    if (i>=0)
-        if (matriz[i][vertice-1])
-            return 1+gradoEntrada(matriz,vertice,i-1);
-        else
-            return gradoEntrada(matriz,vertice,i-1);
-    else
-        return 0;
-}
 /*d) determinar si todos los vértices de un grafo almacenado en media matriz de adyacencia
 (triángulo superior) tienen al menos un vértice adyacente con costo mayor a X (dato).*/
 int todosMarcados(int vector[],int orden){
@@ -79,4 +96,26 @@ int todosTienenCosto(int matriz[][max],int costo,int i,int j,int orden,int marca
         return (todosTienenCosto(matriz,costo,i-1,j-1,orden,marcados)||todosTienenCosto(matriz,costo,i-1,j,orden,marcados));
     }else
         return todosMarcados(marcados,orden);
+}
+
+/*e) generar un vector de registros con grado y vértice para aquellos vértices, de un grafo
+almacenado en una matriz de adyacencia, cuyo grado sea mayor a 3.*/
+
+void generaVector(int matriz[][max],int grados[max],TArray arreglo[],int *n,int i,int j,int orden){
+    if (i>=0){
+        if (j>0){
+            if (matriz[i][j])
+                grados[i]++;
+            generaVector(matriz,grados,arreglo,n,i,j-1,orden);
+        }else{ //FINAL DE FILA
+            if (matriz[i][j])
+                grados[i]++;
+            generaVector(matriz,grados,arreglo,n,i-1,orden-1,orden);
+            if (grados[j]>3){
+                (*n)++;
+                arreglo[*n].vertice=j+1;
+                arreglo[*n].grado=grados[j];
+            }
+        }
+    }
 }
