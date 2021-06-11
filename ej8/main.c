25fafa1b596ae6fd86498ee26d9f9da84563f758
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-06-11 19:43:21 -0300

    correcciones

--- ej8/main.c
+++ ej8/main.c
@@ -58,7 +58,8 @@ adyacencia.*/
 void mayorEntradaLista(TLista lista[max],int grados[max],TLista aux,int i,int orden,int *gMax,int *vMax){//i entra como ORDEN
     if (i>=0){
         if (aux==NULL){ //este es el ultimo elemento de cada elemento del vector
-            mayorEntradaLista(lista,grados,lista[i-1]->sig,i-1,orden,gMax,vMax);
+            if (i>0)
+                mayorEntradaLista(lista,grados,lista[i-1]->sig,i-1,orden,gMax,vMax);
             if (i==0){
                 *gMax=grados[i];
                 *vMax=i+1;
